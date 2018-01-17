3357bf75d964f2f3302451d85793d80b80616dad
Andrew <andykuo1supergreen@gmail.com>
2018-01-16 22:24:40 -0800

    Implemented mogli.js

--- client/app.js
+++ client/app.js
@@ -9,36 +9,49 @@ const canvas = document.querySelector("#content");
 canvas.width = screenWidth;
 canvas.height = screenHeight;
 
+//File Setup
+//if (!window.File || !window.FileReader || !window.FileList)
+//{
+//	throw new Error("Unable to find File API. Your browser or machine may not support it.")
+//}
+
 //WebGL Setup
 const gl = canvas.getContext("webgl");
-
 if (!gl)
 {
-	alert("Unable to initialize WebGL. Your browser or machine may not support it");
-	throw new Error();
+	throw new Error("Unable to initialize WebGL. Your browser or machine may not support it.");
 }
 
 //Clear screen
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
+gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 function start()
 {
 	socket = io.connect('https://localhost');
 	app = new Application();
+	app.doStart();
+
 	app.doNetwork(socket);
 	render(0);
 }
 
-var then = 0;
+//Display frames per second
+setInterval(function(){
+	console.log("FPS: " + frameTime.count);
+	frameTime.count = 0;
+}, 1000);
+
+const frameTime = {delta: 0, then: 0, count: 0};
 function render(now)
 {
 	now *= 0.001;
-	const deltaTime = now - then;
-	then = now;
+	frameTime.delta = now - frameTime.then;
+	frameTime.then = now;
+	++frameTime.count;
 
 	app.doUpdate();
-	app.doRender(gl, deltaTime);
+	app.doRender(gl);
 	requestAnimationFrame(render);
 }
 
---
+++ client/res/def.vsh
@@ -0,0 +1,12 @@
+var vertexShaderDef =
+`
+attribute vec4 aPosition;
+
+uniform mat4 uModelView;
+uniform mat4 uProjection;
+
+void main()
+{
+  gl_Position = uProjection * uModelView * aPosition;
+}
+`
