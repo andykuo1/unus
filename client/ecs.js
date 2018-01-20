f882673889ed70614d6ac7f78521961deaee804b
Andrew <andykuo1supergreen@gmail.com>
2018-01-20 15:58:03 -0800

    Implemented basic ECS

---
+++ client/ecs.js
@@ -0,0 +1,22 @@
+class Entity
+{
+  static _count;
+
+  constructor()
+  {
+    this.id = _count++;
+    this.components = {};
+  }
+
+  addComponent(component)
+  {
+    this.components[component.name] = component;
+    return this;//For method chaining
+  }
+
+  removeComponent(name)
+  {
+    delete this.components[name];
+    return this;//For method chaining
+  }
+}
