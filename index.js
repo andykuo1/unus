1df14a7a2646373f75672aca700a83a0f020a3fe
Andrew <andykuo1supergreen@gmail.com>
2018-01-23 19:32:43 -0800

    Added console to server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.set('port', 8082);
 app.use('/public', express.static(__dirname + '/public'));
@@ -15,12 +15,14 @@ app.get('/gl-matrix/gl-matrix.js', function(request, response) {
 });
 
 //Listening
-var server = app.listen(process.env.PORT || 8082, function() {
+const server = app.listen(process.env.PORT || 8082, function() {
   var port = server.address().port;
   console.log("Server is listening on port " + port + "...");
 });
 
-var io = require('socket.io')(server);
+//Console
+require('./server/console.js');
 
 //Server
-require('./server/server-game.js')(io);
+const io = require('socket.io')(server);
+require('./server/server-game.js');
---
+++ server/console.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (input) => {
+  if ("help" === input)
+  {
+    console.log("Welcome to your help.");
+    console.log("Press \'CTRL-C\' to close the server.");
+  }
+  else
+  {
+    console.log("Unknown command \'" + input + "\'");
+  }
+});
