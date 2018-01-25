19f20e16fb9f660905e4c9d33690621b0abd9468
Andrew <andykuo1supergreen@gmail.com>
2018-01-25 12:57:43 -0800

    Added run script

---
+++ run.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#To run script: 'bash run.bash'
+#If unable to run: 'chmod u+x run.bash' to allow permissions
+
+cd "$(dirname "$0")"
+
+( sleep 1 ; echo "Running Client in...$(pwd)..." ; open http://localhost:8082 ) &
+
+echo "Running Server in...$(pwd)..."
+node ./index.js
